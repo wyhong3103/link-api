c4801490d5743d27342eba466695059bf1bab75a
wyhong3103 <wyhong3103@gmail.com>
2023-07-12 22:06:53 +0800

    Add auth router

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth.controller');
+
+router.post('/login', authController.login);
+
+router.post('/refresh', authController.refresh);
+
+router.post('/register', authController.register);
+
+router.post('/verify-email', authController.verify_email);
+
+router.post('/reset-password', authController.reset_password);
+
+router.post('/verify-reset-password', authController.verify_reset_password);
