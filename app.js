d7f17c8e434d47f11fcc93881e285c7e6e36a8f3
wyhong3103 <wyhong3103@gmail.com>
2023-07-12 14:05:41 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+
+mongoose.set("strictQuery", false);
+
+const mongoDB = process.env.MONGODB;
+
+main().catch(
+    err => console.log(err)
+)
+
+async function main(){
+    await mongoose.connect(mongoDB);
+}
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
