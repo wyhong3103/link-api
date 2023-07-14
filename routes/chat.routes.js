1a7ff51fdfbb3f232b459a5f36f0a91772614829
wyhong3103 <wyhong3103@gmail.com>
2023-07-14 18:57:48 +0800

    Add comments

--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const chatController = require('../controllers/chat.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
+/*
+
+Get messages of user 1 and user 2
+
+Output {
+    messages : [Message]
+}
+
+*/
 router.get('/:roomid', authMiddleware.authorizeUser, chatController.get_chat);
 
 module.exports = router;
