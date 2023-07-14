fa35fa7fecc96d658b2133ccad3107cb3003d55a
wyhong3103 <wyhong3103@gmail.com>
2023-07-14 14:26:12 +0800

    Add read and update post route

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,13 +5,20 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const multer = require('multer');
 const upload = multer({dest : "uploads/"});
 
+/*
+    Get posts from friends and self
+
+*/
+router.get('/', authMiddleware.authorizeUser, postController.get_posts);
+
 router.post('/', authMiddleware.authorizeUser, upload.single('image'), postController.create_post);
-// router.put('/:postid', authMiddleware, postController.update_post);
-// router.delete('/:postid', authMiddleware, postController.delete_post);
-// router.post('/:postid/like', authMiddleware, postController.like_post);
-// router.delete('/:postid/like/', authMiddleware, postController.unlike_post);
-// router.post('/:postid/comment', authMiddleware, postController.comment_post);
-// router.put('/:postid/comment/:commentid', authMiddleware, postController.update_comment);
-// router.delete('/:postid/comment/:commentid', authMiddleware, postController.delete_comment);
+
+router.put('/:postid', authMiddleware.authorizeUser, postController.update_post);
+// router.delete('/:postid', authMiddleware.authorizeUser, postController.delete_post);
+// router.post('/:postid/like', authMiddleware.authorizeUser, postController.like_post);
+// router.delete('/:postid/like/', authMiddleware.authorizeUser, postController.unlike_post);
+// router.post('/:postid/comment', authMiddleware.authorizeUser, postController.comment_post);
+// router.put('/:postid/comment/:commentid', authMiddleware.authorizeUser, postController.update_comment);
+// router.delete('/:postid/comment/:commentid', authMiddleware.authorizeUser, postController.delete_comment);
 
 module.exports = router;
