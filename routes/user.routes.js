ff854314fac52dd9b45cb110890dd3af47ad75e2
wyhong3103 <wyhong3103@gmail.com>
2023-07-14 11:23:01 +0800

    Update middleware name

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
-const userMiddleware = require('../middlewares/user.middleware');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 const multer = require('multer');
 const upload = multer({dest : "uploads/"});
@@ -11,14 +11,14 @@ const upload = multer({dest : "uploads/"});
 Get all the users {first_name, last_name, image}
 
 */
-router.get('/', userMiddleware.authorizeUser, userController.get_users);
+router.get('/', authMiddleware.authorizeUser, userController.get_users);
 
 /*
 
 Get a specific user {first_name, last_name, image, friends, posts, [friend_requests iff user is self]}
 
 */
-router.get('/:userid', userMiddleware.authorizeUser, userController.get_user);
+router.get('/:userid', authMiddleware.authorizeUser, userController.get_user);
 
 
 /*
@@ -26,28 +26,28 @@ router.get('/:userid', userMiddleware.authorizeUser, userController.get_user);
 Send a friend request to :userid
 
 */
-router.post('/:userid/friend-request', userMiddleware.authorizeUser, userController.send_friend_request);
+router.post('/:userid/friend-request', authMiddleware.authorizeUser, userController.send_friend_request);
 
 /*
 
 Accept friend request from :friendid as :userid 
 
 */
-router.post('/:userid/friend-request/:friendid', userMiddleware.authorizeUser, userController.manage_friend_request);
+router.post('/:userid/friend-request/:friendid', authMiddleware.authorizeUser, userController.manage_friend_request);
 
 /*
 
 Delete friend request as either :userid or :friendid, :userid always have to be the one that has the request
 
 */
-router.delete('/:userid/friend-request/:friendid', userMiddleware.authorizeUser, userController.delete_friend_request);
+router.delete('/:userid/friend-request/:friendid', authMiddleware.authorizeUser, userController.delete_friend_request);
 
 /*
 
 Delete friend as either :userid or :friendid
 
 */
-router.delete('/:userid/friend/:friendid', userMiddleware.authorizeUser, userController.delete_friend);
+router.delete('/:userid/friend/:friendid', authMiddleware.authorizeUser, userController.delete_friend);
 
 /*
 
@@ -56,7 +56,7 @@ Input : {
 }
 
 */
-router.put('/:userid/password', userMiddleware.authorizeUser, userController.change_password);
+router.put('/:userid/password', authMiddleware.authorizeUser, userController.change_password);
 
 /*
 
@@ -65,6 +65,6 @@ Input : {
 }
 
 */
-router.put('/:userid', userMiddleware.authorizeUser, upload.single('image') ,userController.update_user_info);
+router.put('/:userid', authMiddleware.authorizeUser, upload.single('image') ,userController.update_user_info);
 
 module.exports = router;
