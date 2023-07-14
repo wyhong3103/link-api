d1281df4caedabc8b25db20432dcd1fcfa9e971e
wyhong3103 <wyhong3103@gmail.com>
2023-07-14 16:57:29 +0800

    Add post router

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authRouter = require('./auth.routes');
 const userRouter = require('./user.routes');
+const postRouter = require('./post.routes');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("Hello");
 });
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
+router.use('/post', postRouter);
 
 module.exports = router;
