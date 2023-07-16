45a783517a1b1df95522f7770ac07125d0b44dc8
wyhong3103 <wyhong3103@gmail.com>
2023-07-16 22:10:23 +0800

    Add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -21,6 +22,7 @@ async function main(){
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
