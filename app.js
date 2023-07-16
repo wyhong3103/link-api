a7262d05a5ec95e11174c689e6ad729264d9ccfb
wyhong3103 <wyhong3103@gmail.com>
2023-07-16 22:40:00 +0800

    Add origin

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ async function main(){
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin : process.env.CLIENT_URL,
+        credentials : true
+    }
+));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
