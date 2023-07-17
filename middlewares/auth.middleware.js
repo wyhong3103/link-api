5629feb6cbe0d60d968a325aac3d4eee81d53c3b
wyhong3103 <wyhong3103@gmail.com>
2023-07-17 15:59:14 +0800

    Fix authorize user middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,29 +1,36 @@
 const authService = require('../services/auth.service');
 
 const authorizeUser = (req, res, next) => {
-    if (typeof req.cookies === 'object' && Object.hasOwnProperty.bind(req.cookies)('accessToken')){
-        const result = authService.verifyToken(req.cookies.accessToken,'access');
-        if (!result.status){
-            if (result.error === 'expired'){
-                req.refresh = true;
-                next();
+    if (typeof req.cookies === 'object'){
+        if (Object.hasOwnProperty.bind(req.cookies)('accessToken')){
+            const result = authService.verifyToken(req.cookies.accessToken,'access');
+            if (!result.status){
+                console.log(result.error);
+                if (result.error !== 'expired'){
+                    res.status(403).json({
+                        status : false,
+                        token : result.error,
+                        error : {result : `Token is ${result.error}.`}
+                    })
+                    return;
+                }
             }else{
-                res.status(403).json({
-                    status : false,
-                    token : result.error,
-                    error : {result : `Token is ${result.error}.`}
-                })
+                req.userid = result.decoded.userid;
+                next();
+                return;
             }
-        }else{
-            req.userid = result.decoded.userid;
-            next();
         }
-    }else {
-        res.status(403).json({
-            status : false,
-            error : {result : 'Please log in.'}
-        });
+        if (Object.hasOwnProperty.bind(req.cookies)('refreshToken')){
+            req.refresh = true;
+            next();
+            return;
+        }
     }
+
+    res.status(403).json({
+        status : false,
+        error : {result : 'Please log in.'}
+    });
 }
 
 module.exports = {
