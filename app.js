e5289be439b303a086188ea87b91b20902b019a1
wyhong3103 <wyhong3103@gmail.com>
2023-07-19 12:30:25 +0800

    Add chat socket

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('./chat-socket');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
--- services/chat.service.js
+++ services/chat.service.js
@@ -1,5 +1,7 @@
+const logger = require('debug')('link-api:chat-service');
 const Message = require('../models/message');
 const Chat = require('../models/chat');
+const User = require('../models/user');
 
 const getRoomID = (a, b) => {
     return (
@@ -10,19 +12,40 @@ const getRoomID = (a, b) => {
     )
 }
 
-const insertMessage = async (user1, user2, message) => {
+const insertMessage = async (selfid, user1, user2, message) => {
+    const user1Obj = await User.findById(user1).exec();
+    const user2Obj = await User.findById(user2).exec();
+
+    if (user1Obj === null || user2Obj === null){ 
+        logger('User not found. Message is ignored.');
+        return;
+    }        
+    if ((selfid !== user1 && selfid !== user2) || (message.author._id !== selfid)){ 
+        logger('No permission. Message is ignored.');
+        return;
+    }
+
     let chat = await Chat.findOne({room : getRoomID(user1, user2)}).exec();
 
     if (chat === null){
         chat = new Chat({
-            room : getRoomID(user2, user2),
+            room : getRoomID(user1, user2),
             users : [
                 user1, user2
-            ]
+            ],
+            messages : []
         });
     }
 
-    const msg = await(new Message(message)).save();
+    const msg = await(new Message(
+    {
+        content : message.content,
+        markdown : message.markdown,
+        math : message.math,
+        author : message.author._id,
+        date : Date.now()
+    }
+    )).save();
     chat.messages.push(msg._id);
     await chat.save();
 }
